e2b0a45e5bb91d5d5ae660bff16c1245371284e6
Jahnea Bush <jbush0811@floridapoly.edu>
2025-08-22 14:36:10 +0000

    Created the first program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,23 @@
+/*
+
+Multi-line comment. 
+08/20/25
+Jahnea
+the first porogram in C.
+*/
+
+// One line comment 
+
+// preprocessor directive
+// stdio.h- standard input output
+// .h - header file 
+#include <stdio.h>  
+
+// Create main function
+int main(void){
+    printf("Hello World\n"); // \n (new line) - escape sequence
+
+    printf("I\n am \nready \nto \ncode! \n");
+    
+    return 0;
+}
